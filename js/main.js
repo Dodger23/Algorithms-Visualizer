8fbcbbf45634d16e5c1956deb19a2782e3e98c2c
Tarek.Hassan <tarekhassan3874@gmail.com>
2020-01-25 00:23:23 +0200

    Linked the Selection sort from the Home Page

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ var algorithmsList = [
         pageLink : 'file:///C:/Users/Dodger/Desktop/Project/Algorithms%20Visualizer/Algotithms/Bubble-sort/index.html' 
     },
     {
-        name : 'Bubble Sort',
-        about : 'Repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.',
+        name : 'Selection Sort',
+        about : 'an in-place comparison sort. It has O(n²) time complexity, making it inefficient on large lists',
         imgPath : 'images/sorting.gif' ,
-        pageLink : '#' 
+        pageLink : 'file:///C:/Users/Dodger/Desktop/Project/Algorithms%20Visualizer/Algotithms/Selection-sort/index.html' 
     },
     {
         name : 'Bubble Sort',
